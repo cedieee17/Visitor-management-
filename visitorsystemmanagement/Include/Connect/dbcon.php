35d641f53541c9c7f709ae358a77f7dfab039083
Calixxx01 <149688175+Calixxx01@users.noreply.github.com>
2023-11-04 14:25:11 +0800

    Delete visitorsystemmanagement directory

--- visitorsystemmanagement/Include/Connect/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $pdoConnect = new PDO("mysql:host=localhost;dbname:dbtest","root","");
-    $pdoConnect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $exc) {
-    echo $exc->getMessage();
-}
-?>
