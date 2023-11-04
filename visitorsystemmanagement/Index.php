2cdb322f814f4aab52bff4383631f6f3d884596b
kbautstaaa <149758681+kbautstaaa@users.noreply.github.com>
2023-11-05 03:51:31 +0800

    Update Index.php

--- visitorsystemmanagement/Index.php
+++ visitorsystemmanagement/Index.php
@@ -7,20 +7,20 @@ Try {
  $pdoConnect->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if (isset($_POST["login"])) {
-if (empty($_POST["Email"]) || empty($_POST["Password"])) { 
+if (empty($_POST["UserName"]) || empty($_POST[“Password”])) { 
 $message = '<label> All fields are required </label>';
  } else {
 
-$pdoQuery = "SELECT * FROM dbtest WHERE Email :Email";
-$pdoResult = $pdoConnect->prepare($pdoQuery);
-$pdoResult->execute(['Email' => $_POST["Email"]]);
+$pdoQuery = "SELECT * FROM dbtest WHERE UserName :UserName";
+$pdoResult = SpdoConnect->prepare($pdoQuery);
+$pdoResult->execute(['UserName' => $_POST[“UserName”]]);
 
 $user = SpdoResult->fetch();
 
 If ($user && password_verify($_POST["Password"], $user["Password"])) { 
-    $_SESSION["Email"] = $_POST["Email"];
+    $_SESSION["UserName"] = $_POST["UserName"];
 
-$loggedInUser = $_SESSION["Email"];
+$loggedInUser = $_SESSION["UserName"];
 $pdoQuery = "INSERT INTO audit_trail('action', 'user') VALUES ('User logged in', :user)"; 
 $pdoResult = $pdoConnect->prepare($pdoQuery); 
 $pdoResult->execute([':user' => $loggedInUser]);
@@ -31,40 +31,48 @@ $message = '<label>Wrong data</label>';
 }
 }
 } catch (PDOException $error) {
-$message = $error ->getMessage();
+$message = $error ->getmessage();
 }
 
 
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>VISITOR SYSTEM MANAGEMENT</title>
-<link rel=”stylesheet” type=”text/css” href=”./Include/Styles/Styles.css”>
+    <meta charset ="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+<link rel="stylesheet"  type="text/css" href= "./include/style/Style.css">
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-<br />
-<div style=”width:500px;”>
-
-<?php
-
-If (isset($message)) {
-Echo '<label>'.$message.'</label>';
-}
-?>
-<h3 Align=””>VISITOR SYSTEM MANAGEMENT</h3><br/>
-<form method=”post”>
-Username
-<input type=”text” name="Email">
-<br />
-Password
-<input type=”password” name=”Password”>
-<br />
-<input type="submit" name="login" value="Login">
-</form>
-
-<a href="register.php">  Add user</a>
-</div>
-<br>
+    <div class="wrapper">
+    <form action="login.php" method="post">    
+    
+       
+            <div class="input-box">
+            <input type="text" placeholder ="Username" required>
+            <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+            <input type="password" placeholder ="Password" required>
+            <i class='bx bxs-lock-alt'></i>
+            </div>
+        
+            <div class="remember-forgot">
+                <label><input type="checkbox"> Remember me </label>
+                <a href="forgot_password.php">Forgot password?</a>
+            </div>
+            
+            <button type="submit" class="btn">Login</button>
+        
+            <div class="register-link">
+            <p>Don't have an account? <a href="register.php">Register</a></p>
+            </div>
+        
+        </form>
+    </div>
 </body>
 </html>
