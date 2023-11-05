b100ea46d18ea5ed4d428cb8a6d2802a180144c9
Calixxx01 <149688175+Calixxx01@users.noreply.github.com>
2023-11-05 22:19:21 +0800

    Add files via upload

---
+++ visitorsystemmanagement/Student/Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="Stylesheet" type="text/css" href="./style.css">
+    </head>
+<body>
+<div class="container">
+    <img class ="dhvsulogo" src="logooo.jpg" alt="ccs">
+    <h2>Student Registration</h2>
+    <div class="wrapper">
+</div>
+    <form method="POST" action="function.php" >
+        <br>
+        <b>Student ID: </b> <input type="StudentId" name="StudentId"> <br />
+        <br>
+        <b>Email: </b> <input type="email" name="Email"> <br />
+        <br>
+        <b>Password: </b> <input type="password" name="Password"> <br />
+        <br>
+        <input type="submit" name="register" value="Register">
+             
+    </form>
+</div>
+</body>
+</html>
---
+++ visitorsystemmanagement/Visitor/Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="Stylesheet" type="text/css" href="./style.css">
+    </head>
+<body>
+<div class="container">
+    <img class ="dhvsulogo" src="logooo.jpg" alt="ccs">
+    <h2>Student Registration</h2>
+    <div class="wrapper">
+</div>
+    <form method="POST" action="function.php" >
+        <br>
+        <b>Fullname: </b> <input type="text" name="FullName" > <br />
+        <br>
+        <b>Contact: </b> <input type="int" name="Contact" > <br />
+        <br>
+        <b>Purpose: </b> <input type="text" name="Purpose" > <br />
+        <br>
+        <input type="submit" name="Visitor" value="Submit">
+             
+    </form>
+</div>
+</body>
+</html>
