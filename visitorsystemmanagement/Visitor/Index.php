afddb2ed284181166db9297ad6ec93ab96f83a98
kbautstaaa <149758681+kbautstaaa@users.noreply.github.com>
2023-11-05 23:59:58 +0800

    Update Index.php

--- visitorsystemmanagement/Visitor/Index.php
+++ visitorsystemmanagement/Visitor/Index.php
@@ -7,14 +7,14 @@
 <body>
 <div class="container">
     <img class ="dhvsulogo" src="logooo.jpg" alt="ccs">
-    <h2>Student Registration</h2>
+    <h2>Visitor Registration</h2>
     <div class="wrapper">
 </div>
     <form method="POST" action="function.php" >
         <br>
         <b>Fullname: </b> <input type="text" name="FullName" > <br />
         <br>
-        <b>Contact: </b> <input type="int" name="Contact" > <br />
+        <b>Contact: </b> <input type="number" name="Contact" require> <br />
         <br>
         <b>Purpose: </b> <input type="text" name="Purpose" > <br />
         <br>
